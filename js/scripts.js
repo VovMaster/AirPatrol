5b92ee9bd9bfb97060871b7b258400a52e2c7e7c
vladimir <vovmaster2@gmail.com>
2017-06-08 14:06:11 +0300

    trtyty

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,56 @@ $(document).ready(function() {
 	    }
 	});
 	$(".dropdown-language-title").click(function(){
-
+		$(this).parent().toggleClass('open');
 	});
+	$('.products-menu-link').hover(function(){
+		$('.products-menu').show();
+	});
+
+
+
+	var intervalID;
+	$('.products-menu-link').hover(
+		function() {
+			clearInterval(intervalID);
+			$('.products-menu').show();
+		}, function() {
+		    closeDropdownHover();
+		}
+	);
+
+	$('.products-menu').hover(
+		function() {
+			clearInterval(intervalID);
+		}, function() {
+		    closeDropdownHover();
+		}
+	);
+
+
+	function closeDropdownHover(){
+		intervalID = setInterval(function(){
+			$('.products-menu').hide(); 
+		}, 400);
+
+	}
+
+	$('.menu-mobile-top a').click(function(){
+		var mobileHref = $(this).attr('href');
+		$(mobileHref).show();
+		return false
+	});
+
+	$('.back-menu-js').click(function(){
+		$('.menu-mobile-dropdown').hide();
+		return false
+	});
+
+
+	$('.menu-button').click(function(){
+		$('html').toggleClass('menu-mobile-open');
+		return false
+	});
+
+
 });
