ec25514747207f124cc9bf2663ae66c52ab98921
vladimir <vovmaster2@gmail.com>
2017-06-08 08:14:48 +0300

    upasdf

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+	$(document).mouseup(function (e) {
+	    var container = $(".dropdown-language");
+	    if (container.has(e.target).length === 0){
+	        container.removeClass('open');
+	    }
+	});
+	$(".dropdown-language-title").click(function(){
 
+	});
 });
